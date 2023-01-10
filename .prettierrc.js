c7cf711858762ccb545324fbce20cbe989a4d985
younsoo <tweatershin@gmail.com>
2023-01-10 09:53:48 +0900

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "printWidth": 120,
+  "useTabs": true,
+  "semi": true,
+  "singleQuote": false,
+  "quoteProps": "as-needed",
+  "jsxSingleQuote": false,
+  "arrowParens": "avoid",
+  "requirePragma": false,
+  "insertPragma": false,
+  "proseWrap": "preserve",
+  "htmlWhitespaceSensitivity": "css",
+  "endOfLine": "auto",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": true,
+  "trailingComma": "all",
+  "parser": "babel",
+  "tabWidth": 2
+}
+
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HelloWorld() {
+	return (
+		<div style={{ textAlign: "center", color: "blue", fontWeight: 800 }}>
+			<h1>Hello, World!</h1>
+			<p>This is my first React Application.</p>
+		</div>
+	);
+}
