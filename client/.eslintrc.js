8aa208036e6919ab8da81e56a5daad5e83efca93
BeTweak <ys@betweak.com>
2023-01-12 22:46:02 +0900

    # react client init

---
+++ client/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': 'warn',
+    'prettier/prettier': 'warn',
+    'no-param-reassign': 0,
+    'arrow-parens': 0,
+    'max-len': [
+      'error',
+      {
+        code: 120,
+      },
+    ],
+    'comma-dangle': 0,
+    'object-curly-newline': 0,
+    'no-unused-vars': 'warn',
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
+};
